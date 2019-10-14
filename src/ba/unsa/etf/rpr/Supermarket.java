27973fa586d72da9c934e1d7131b262eeebaa1a4
dbasalic1 <dbasalic1@etf.unsa.ba>
2019-10-14 17:19:01 +0200

    dodane metode u supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli;
+    private int brojArtikala;
+
+    public Supermarket() {
+        brojArtikala = 0;
+        artikli = new Artikl[1000];
+    }
+
+    public void dodajArtikl(Artikl a) {
+        artikli[brojArtikala++] = a;
+    }
 }
