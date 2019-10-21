8f340ea597b869c9d1862cfa1762a9c0db62fe22
dbasalic1 <dbasalic1@etf.unsa.ba>
2019-10-21 16:34:32 +0200

    testovi

--- test/ba/unsa/etf/rpr/KorpaTest.java
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -1,17 +1,28 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KorpaTest {
 
 
-
-    @org.junit.jupiter.api.Test
-    void izbaciArtiklSaKodom() {
+    @Test
+    void daLiSeIspravnoDodajeArtikl() {
         Korpa k = new Korpa();
-        k.dodajArtikl(new Artikl("a", 1, "1"));
-        k.dodajArtikl(new Artikl("b", 1, "2"));
-        Artikl a = k.izbaciArtiklSaKodom("2");
-        assertEquals("2", a.getKod());
+        k.dodajArtikl(new Artikl("naziv",25,"kod"));
+        Artikl[] artikli = k.getArtikli();
+        assertAll( () -> {
+            assertNotNull(artikli[0]);
+            assertEquals("naziv", artikli[0].getNaziv());
+        });
+    }
+
+    @Test
+    void daLiCePrekoracitiBrojArtikala() {
+        assertDoesNotThrow( ()-> {
+            Korpa k = new Korpa();
+            for (int i = 0; i < 2000; i++) k.dodajArtikl(new Artikl("naziv" + i, i, "kod" + i));
+        });
     }
 }
